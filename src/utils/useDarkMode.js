5ebd59f867167d4d8eae3898c74c0202a7c41f99
Mayowa Awoyomi <mayowaawoyomi@gmail.com>
2022-07-21 03:28:28 +0100

    message

--- src/utils/useDarkMode.js
+++ src/utils/useDarkMode.js
@@ -1,8 +1,8 @@
-import { useState, useEffect, useRef } from "react";
+import { useMemo } from "react";
+import { useState } from "react";
 
 export function useDarkMode() {
     const [theme, setTheme] = useState("light")
-    let onceRef = useRef(false)
 
     const setMode = (mode) => {
         localStorage.setItem("theme", mode)
@@ -11,20 +11,16 @@ export function useDarkMode() {
 
     const Toggle = () => (theme === "dark") ? setMode("light") : setMode("dark")
 
-    useEffect(() => {
-        if(onceRef.current) return;
-
-        //get the theme in the localstorage 
+    useMemo(() => {
         const themeVal = localStorage.getItem("theme")
-
-    //if there is a theme in the localStrage use it else set it as light
         themeVal ? setTheme(themeVal) : setMode("light")
-
-        onceRef.current = true;
 
     }, [])
 
-    return{
+
+
+
+    return {
         theme,
         Toggle
     }
